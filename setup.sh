d72cbf2388303b225d1459b8427b590e31dca653
Oleksandr B. <oleksandr.bi@paymentop.com>
2024-01-18 10:04:01 +0200

    Update config

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ~/.config/core-config/lazygit/config.yml ~/.config/lazygit/config.yml
+ln -s ~/.config/core-config/gitconfig.toml ~/.gitconfig
+ln -s ~/.config/core-config/zshrc ~/.zshrc
+ln -s ~/.config/core-config/tmux.conf ~/.tmux.conf
